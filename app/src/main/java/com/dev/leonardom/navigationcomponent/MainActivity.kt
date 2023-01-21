9488d110f584f1a9fc86e4d5b3a6087640e91327
Oscar-725 <oscar.rosalipo@gmail.com>
2023-01-21 15:25:54 -0600

    Initial commit

---
+++ app/src/main/java/com/dev/leonardom/navigationcomponent/MainActivity.kt
@@ -0,0 +1,61 @@
+package com.dev.leonardom.navigationcomponent
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.dev.leonardom.navigationcomponent.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    //Creamos variable global para instanciar nuestro navController
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Creamos variable para definir el contenedor de fragmentos
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        //iniciamos navController vinculandolo con nuestro navHostController
+        navController = navHostFragment.findNavController()
+
+        //Toolbar
+        setSupportActionBar(binding.toolbar)
+        setupActionBarWithNavController(navController)
+
+    }
+
+    //Para activar la flecha de retroseso al fragment anterior
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+}
+
+
+//Creamos comunicacion entre nuestro menu y fragmentos por medio del navComtroller
+//1.- creamos variable navController
+//2.- configuramos nuestro tool bar
+
+
+
+
+
+
